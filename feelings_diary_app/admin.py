6e50c70a1139b4f01698ea9bf6bc2a2c3e930b7c
nicola-laurel-willis <124948483+nicola-laurel-willis@users.noreply.github.com>
2023-04-22 00:14:00 +0100

    added DiaryEntry model and made migrations. added urlconfs and a test index view

--- feelings_diary_app/admin.py
+++ feelings_diary_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Teacher
+from .models import Teacher, DiaryEntry
 
 # Register your models here.
 
 admin.site.register(Teacher)
+admin.site.register(DiaryEntry)
--- feelings_diary_app/models.py
+++ feelings_diary_app/models.py
@@ -5,3 +5,10 @@ class Teacher(models.Model):
     age = models.IntegerField()
     def __str__(self):
         return self.name
+      
+      
+class DiaryEntry(models.Model):
+    text = models.CharField(max_length=80)
+    pub_date = models.DateTimeField("date published")
+    def __str__(self):
+        return f"{self.text}, {self.pub_date}"
